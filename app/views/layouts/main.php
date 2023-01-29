6aa4c537584feb915c2043269cd6cc2a6a0cfe10
qhawe <qhawe@kingston-enterprises.net>
2023-01-29 22:46:03 +0200

    Revert "added nav bar"
    This reverts commit a32664be1325451dfb0001db5d85110343cc258e.

--- app/views/layouts/main.php
+++ app/views/layouts/main.php
@@ -8,22 +8,6 @@
   <link rel="stylesheet" href="/css/tailwind.css" />
 </head>
 <body class="bg-blue-200">
-  <nav class="flex flex-row justify-between m-0 p-5 text-orange-500 font-bold underline">
-    <div>
-      <a href="/">Home</a>
-    </div>
-    <div >
-      <ul class="flex flex-row justify-between">
-        <li class="mx-2">
-          <a href="/auth/login">Login</a>
-        </li>
-
-        <li class="mx-2">
-          <a href="/auth/register">Register</a>
-        </li>
-      </ul>
-    </div>
-  </nav>
   {{content}}
 </body>
 </html>
